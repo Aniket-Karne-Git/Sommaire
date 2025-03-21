77b3d99496554a199870842fc24b98d4219957f8
Aniket-Karne-Git <ankarne@paktolus.com>
2025-03-21 22:50:56 +0530

    added validations for file using zod

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,5 +1,49 @@
+"use client";
+
+import { z } from "zod";
+import UploadFormInput from "./upload-form-input";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Invalid File" })
+    .refine(
+      (file) => file.size <= 20 * 1024 * 1024,
+      "File size must be less than 20MB"
+    )
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "File must be a pdf"
+    ),
+});
+
 const UploadForm = () => {
-  return <div>UploadForm</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted");
+    const formdata = new FormData(e.currentTarget);
+    const file = formdata.get("file") as File;
+
+    // validating the fields
+    const validatedFields = schema.safeParse({ file });
+    console.log(validatedFields);
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid File"
+      );
+      return;
+    }
+    // Schema with zod
+    // uplpad the file to uploadthing
+    // parse the pdf using lang chain
+    // summarize the pdf using AI
+    // save the summary to the database
+    // redirect to the [id] summary page
+  };
+  return (
+    <div className="flex flex-col gap-8 w-full max-w-2xl mx-auto">
+      <UploadFormInput onSubmit={handleSubmit} />
+    </div>
+  );
 };
 
 export default UploadForm;
