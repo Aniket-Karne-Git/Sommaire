fb7423e22cf544e67422db4cab592f031a3d32fd
Aniket-Karne-Git <ankarne@paktolus.com>
2025-03-24 12:17:36 +0530

    removed unused console logs

--- lib/langchain.ts
+++ lib/langchain.ts
@@ -2,9 +2,7 @@ import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 
 export async function fetchAndExtractPdfText(fileUrl: string) {
   const response = await fetch(fileUrl);
-  console.log("response from FileUrl ", response);
   const blob = await response.blob();
-  console.log("Response Blob", blob);
 
   const arrayBuffer = await blob.arrayBuffer();
 
